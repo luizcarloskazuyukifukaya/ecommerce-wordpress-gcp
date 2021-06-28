1d80ac0067405f603261babaf5ae8c14aa310f34
Luiz Carlos Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2021-06-28 21:16:22 +0900

    Example with WordPress on GKE and CloudSQL

---
+++ 2-wordpress/k8scluster.tf
@@ -0,0 +1,28 @@
+// Configure the Google Cloud provider
+provider "google" {
+ project = var.project
+ region = var.region
+}
+
+resource "google_container_cluster" "gkecluster" {
+  name               = "${var.prefix_name}-k8scluster"
+  location           = var.region
+  initial_node_count = 1
+
+  //network    = google_compute_network.vpc_network.name
+  network = "${var.prefix_name}-network"
+
+  //subnetwork = google_compute_subnetwork.network.name
+  subnetwork = "${var.prefix_name}-subnetwork"
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
---
+++ managed-cloud-sql/cloudsql.tf
@@ -0,0 +1,29 @@
+// Configure the Google Cloud provider
+provider "google" {
+ project = var.project
+ region = var.region
+}
+
+resource "google_sql_database_instance" "master" {
+  name = "${var.prefix_name}-mysql"
+  database_version = "MYSQL_5_7"
+  region       = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name      = "${var.prefix_name}-database"
+  instance  = "${google_sql_database_instance.master.name}"
+  charset   = "sjis"
+  collation = "sjis_japanese_ci"
+}
+
+resource "google_sql_user" "users" {
+  name     = var.mysql_user
+  instance = "${google_sql_database_instance.master.name}"
+  host     = "%"
+  password = var.mysql_password
+}
